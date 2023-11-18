86a5083fde19060d09223675d146a2bbce2cd78e
Maximiliano Greco <mmngreco@gmail.com>
2023-11-18 16:57:11 +0100

    enh: refactor

--- takt.py
+++ takt.py
@@ -34,11 +34,18 @@ License
 -------
 MIT License
 
+TODO
+----
+- [o] Add plug-in system
+- [ ] Add a command to create a gantt chart using mermaid
+- [ ] Add a command to commit changes to git
+- [ ] Implement multiple files (projects)
+
 """
+from datetime import date
 import os
 import subprocess
 from collections import defaultdict
-from datetime import datetime
 
 import pandas as pd
 import typer
@@ -60,93 +67,12 @@ COLUMNS = [
     KIND,
     NOTES,
 ]
+SECONDS_TO_HOURS = 1 / 3600
 
 
-
-class Aggregator:
-    def __init__(self, records: list[dict]):
-        self.records = records
-
-    def calculate(self, period: str | None = None) -> dict[str, float]:
-        summary_dict = defaultdict(lambda: [0.0, set()])
-        last_in_time = None
-        last_out_time = None
-        if self.records[0][KIND] == "in":
-            new_record = {
-                KIND: "out",
-                TIMESTAMP: pd.Timestamp.now(),
-            }
-            self.records.insert(0, new_record)
-            msg = "NOTE: Last out was inferred using `Timestamp.now()`."
-            console.print(msg)
-
-        for record in self.records:
-            kind = record[KIND]
-            timestamp = record[TIMESTAMP]
-            week = timestamp.strftime('%U')
-            year = timestamp.year
-            month = timestamp.month
-
-            if period == 'wtd':
-                group_by = f"{year}-W{week}"
-            elif period == 'ytd':
-                group_by = f"{year}"
-            elif period == 'mtd':
-                group_by = f"{year}-M{month}"
-            else:
-                group_by = timestamp.date().isoformat()
-
-            if kind == 'in':
-                last_in_time = timestamp
-            else:
-                last_out_time = timestamp
-
-            if last_in_time and last_out_time:
-                time_diff = last_out_time - last_in_time
-                hours = time_diff.total_seconds() / 3600
-                summary_dict[group_by][0] += hours
-                summary_dict[group_by][1].add(last_out_time.date())
-                last_in_time = None
-                last_out_time = None
-
-        return summary_dict
-
-
-def format_time(hours: float) -> str:
-    h = str(int(hours)).zfill(2)
-    m = str(int((hours - int(hours)) * 60)).zfill(2)
-    return f"{h}:{m}"
-
-
-def display_summary_table(summary_dict: dict[str, tuple[float, int]], limit=10):
-    table = Table(show_header=True, header_style="bold magenta")
-    table.add_column("Date", style="dim")
-    table.add_column("Hours", style="dim")
-    table.add_column("Days", style="dim")  # Nueva columna
-    table.add_column("Avg Hours", style="dim")  # Nueva columna
-
-    for i, (day, (total_hours, days)) in enumerate(summary_dict.items()):
-        num_days = len(days)
-        total_hours_str = format_time(total_hours)
-        avg_hours = total_hours / num_days if num_days else 0  # Media de horas
-        avg_hours_str = format_time(avg_hours)
-
-        table.add_row(day, total_hours_str, str(num_days), avg_hours_str)
-        if i >= limit:
-            break
-
-    console.print(table)
-
-
-def strip_values(df: pd.DataFrame) -> pd.DataFrame:
-    df.columns = df.columns.str.strip()
-    for c in df.columns:
-        try:
-            df[c] = df[c].str.strip()
-        except AttributeError:
-            pass
-
-    return df
+class FileRow(dict):
+    def __init__(self, timestamp, kind, notes):
+        super().__init__(timestamp=timestamp, kind=kind, notes=notes)
 
 
 class FileManager:
@@ -177,7 +103,7 @@ class FileManager:
         return False
 
 
-    def load(self, nrows=None) -> dict[str, float]:
+    def load(self, nrows=None) -> list[dict[str, float]]:
         data = self.read(nrows=nrows)
         return data.to_dict('records')
 
@@ -210,56 +136,244 @@ class FileManager:
         return df[(df['week'] == str(week)) & (df['year'] == year)]
 
 
-@app.command()
-def check(notes: str = "", filename: str = FILE_NAME):
+
+class DailyRef:
+    @staticmethod
+    def group(timestamp):
+        group_by = timestamp.date().isoformat()
+        return group_by
+
+class WeekRef:
+    @staticmethod
+    def group(timestamp):
+        year = timestamp.year
+        week = timestamp.strftime('%U')
+        group_by = f"{year}-W{week}"
+        return group_by
+
+
+class YearRef:
+    @staticmethod
+    def group(timestamp):
+        year = timestamp.year
+        group_by = f"{year}"
+        return group_by
+
+
+class MonthRef:
+    @staticmethod
+    def group(timestamp):
+        year = timestamp.year
+        month = timestamp.month
+        group_by = f"{year}-M{month}"
+        return group_by
+
+
+class AggRow:
+
+    def __init__(self, group: str, total: float | int, collection: set[date]):
+        self.group = group
+        self.total = total
+        self.collection = collection
+
+    def to_dict(self) -> dict[str, str | float | set[date]]:
+        return {
+            'time_ref': self.group,
+            'total': self.total,
+            'days': self.collection,
+        }
+
+class Aggregator:
+    def __init__(self, period: str = 'daily'):
+        self.period = period
+        if period == 'wtd':
+            self.time_agg = WeekRef.group
+        elif period == 'ytd':
+            self.time_agg = YearRef.group
+        elif period == 'mtd':
+            self.time_agg = MonthRef.group
+        elif period == 'daily':
+            self.time_agg = DailyRef.group
+        else:
+            raise ValueError(f"Period {period} not supported.")
+
+    @staticmethod
+    def infer_last_out(records):
+        if records[0][KIND] == "in":
+            new_record = {
+                KIND: "out",
+                TIMESTAMP: pd.Timestamp.now(),
+            }
+            records.insert(0, new_record)
+            msg = "NOTE: Last out was inferred using `Timestamp.now()`."
+            console.print(msg)
+        return records
+
+    def calculate(self, records: list[FileRow]) -> list[AggRow]:
+        last_in_time = None
+        last_out_time = None
+
+        records = self.infer_last_out(records)
+        row_collection = []
+
+        # loop over records
+        for record in records:
+            # get variables
+            kind = record[KIND]
+            timestamp = record[TIMESTAMP]
+            note = record[NOTES]
+
+            # update variables
+            if kind == 'in':
+                last_in_time = timestamp
+            else:
+                last_out_time = timestamp
+
+            # only consider pairs of in/out
+            if last_in_time and last_out_time:
+
+                # calculate hours
+                time_diff = last_out_time - last_in_time
+                total_seconds = time_diff.total_seconds()
+                total_hours = total_seconds * SECONDS_TO_HOURS
+                group_by = self.time_agg(timestamp)
+                date = timestamp.date()
+                # save data
+                new_row = dict(group=group_by, hours=total_hours, dates=set([date]))
+                # breakpoint()
+                if not row_collection:
+                    row_collection.append(new_row)
+                else:
+                    pre_row = row_collection[-1]
+                    if new_row["group"] == pre_row["group"]:
+                        pre_row["hours"] += new_row["hours"]
+                        pre_row["dates"].update(new_row["dates"])
+                    else:
+                        row_collection.append(new_row)
+
+                # reset variables
+                last_in_time = None
+                last_out_time = None
+
+        return row_collection
+
+
+
+def format_time(hours: float) -> str:
+    h = str(int(hours)).zfill(2)
+    m = str(int((hours - int(hours)) * 60)).zfill(2)
+    return f"{h}:{m}"
+
+
+def display_summary_table(summary_dict: list[dict[str, str | float | int]], limit=10):
+    table = Table(show_header=True, header_style="bold magenta")
+    table.add_column("Date", style="dim")
+    table.add_column("Hours", style="dim")
+    table.add_column("N.Days", style="dim")       # Nueva columna
+    table.add_column("Avg Hours", style="dim")  # Nueva columna
+
+    for i, row in enumerate(summary_dict):
+        day = row['group']
+        total_hours = row['hours']
+        dates = row['dates']
+        nobs = len(dates)
+
+        total_hours_str = format_time(total_hours)
+        avg_hours = total_hours / nobs if nobs else 0  # Media de horas
+        avg_hours_str = format_time(avg_hours)
+
+        table.add_row(day, total_hours_str, str(nobs), avg_hours_str)
+        if i >= limit:
+            break
+
+    console.print(table)
+
+
+def strip_values(df: pd.DataFrame) -> pd.DataFrame:
+    df.columns = df.columns.str.strip()
+    for c in df.columns:
+        try:
+            df[c] = df[c].str.strip()
+        except AttributeError:
+            pass
+    return df
+
+
+class Takt:
+    def __init__(self):
+        self.row = FileRow
+        self.filename = FILE_NAME
+        self._file_manager = None
+        self._aggregator = None
+
+    @property
+    def file_manager(self):
+        file_manager = self._file_manager
+        if file_manager is None:
+            file_manager = FileManager(self.filename)
+            self._file_manager = file_manager
+        return file_manager
+
+    def records(self, nrows=None):
+        file_manager = self.file_manager
+        return file_manager.load(nrows=nrows)
+
+    def insert(self, timestamp, kind, notes):
+        file_manager = self.file_manager
+        file_manager.insert(**self.row(timestamp, kind, notes))
+
+    def first(self):
+        file_manager = self.file_manager
+        return file_manager.first()
+
+    def aggregate(self, period=None):
+        aggregator = Aggregator(period)
+        records = self.records()
+        return aggregator.calculate(records)
+
+    @staticmethod
+    def command(*args, **kwargs):
+        return app.command(*args, **kwargs)
+
+    @staticmethod
+    def print(msg, style=None):
+        console.print(msg, style=style)
+
+
+@Takt.command()
+def check(notes: str = ""):
     """
     Check in or out.
     """
-    file_manager = FileManager(filename)
-    # NOTE: sorted by timestamp in descending order (most recent first)
-    last_kind = file_manager.first()
+    t  = Takt()
+    timestamp = pd.Timestamp.now()
+    last_kind = t.first()
+    # infer kind
     if last_kind is None or last_kind[KIND] == 'out':
         kind = 'in'
     else:
         kind = 'out'
-    # build record
-    timestamp = pd.Timestamp.now()
-    # insert record
-    file_manager.insert(
-        **{
-            TIMESTAMP: timestamp,
-            KIND: kind,
-            NOTES: notes,
-        }
-    )
-    console.print(f"Check {kind} at {timestamp}", style="green")
+    t.insert(timestamp, kind, notes)
+    t.print(f"Check {kind} at {timestamp}", style="green")
 
 
 @app.command()
-def display(filename: str = FILE_NAME):
+def display():
     """
     Show all records.
     """
-    file_manager = FileManager(filename)
-    data = file_manager.read()
+    t = Takt()
+    data = t.records()
+
     table = Table(show_header=True, header_style="bold magenta")
-    for column in data.columns:
+    for column in data[0].keys():
         table.add_column(column, style="dim")
-    for row in data.to_dict('records'):
+    for row in data:
         timestamp, kind, notes = row.values()
         timestamp = str(timestamp)
         table.add_row(timestamp, kind, notes)
-    console.print(table)
 
-
-@app.command()
-def clear(filename: str = FILE_NAME):
-    """
-    Remove all records from the records file.
-    """
-    with open(filename, 'w', newline='') as _:
-        pass
-    console.print("All records have been cleared.", style="red")
+    t.print(table)
 
 
 @app.command()
@@ -279,30 +393,13 @@ def edit(filename: str = FILE_NAME):
         )
 
 
-@app.command("import")
-def import_csv(source: str, target: str = FILE_NAME):
-    """
-    Import records from source to target.
-    """
-    fm = FileManager(target)
-    try:
-        fm.import_from(source)
-        console.print(
-            f"Data imported form {source} to {target}.", style="green"
-        )
-    except Exception as e:
-        console.print(f"Error: {e}", style="red")
-
-
 @app.command()
-def summary(filename: str = FILE_NAME):
+def summary():
     """
     Daily summary.
     """
-    file_manager = FileManager(filename)
-    records = file_manager.load()
-    aggregator = Aggregator(records)
-    summary_dict = aggregator.calculate()
+    t = Takt()
+    summary_dict = t.aggregate(period='daily')
     display_summary_table(summary_dict)
 
 
@@ -311,40 +408,43 @@ def wtd(filename: str = FILE_NAME):
     """
     Week to date summary.
     """
-    file_manager = FileManager(filename)
-    records = file_manager.load()
-    aggregator = Aggregator(records)
-    summary_dict = aggregator.calculate('wtd')
-    display_summary_table(summary_dict)
+    t = Takt()
+    list_dict = t.aggregate(period='wtd')
+    display_summary_table(list_dict)
 
 
 @app.command()
-def ytd(filename: str = FILE_NAME):
+def ytd():
     """
     Year to date summary.
     """
-    file_manager = FileManager(filename)
-    records = file_manager.load()
-    aggregator = Aggregator(records)
-    summary_dict = aggregator.calculate('ytd')
-    display_summary_table(summary_dict)
+    t = Takt()
+    list_dict = t.aggregate(period='ytd')
+    display_summary_table(list_dict)
 
 
 @app.command()
-def mtd(filename: str = FILE_NAME):
+def mtd():
     """
     Month to date summary.
     """
-    file_manager = FileManager(filename)
-    records = file_manager.load()
-    aggregator = Aggregator(records)
-    summary_dict = aggregator.calculate('mtd')
+    t = Takt()
+    summary_dict = t.aggregate(period='mtd')
     display_summary_table(summary_dict)
-
-# TODO: Add a command to create a gantt chart using mermaid
-# TODO: Add a command to commit changes to git
-# TODO: Implement multiple files (projects)
 
 
 if __name__ == "__main__":
+    import importlib
+    import pkgutil
+
+    discovered_plugins = {
+        name: importlib.import_module(name)
+        for _, name, _
+        in pkgutil.iter_modules()
+        if name.startswith('takt_')
+    }
+
+    for name, plugin in discovered_plugins.items():
+        app.add_typer(plugin.app)
+
     app()
